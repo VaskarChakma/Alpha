0fe26a70893befbb8ab94d18076fc160f263b38c
Vaskar Chakma <74909734+VaskarChakma@users.noreply.github.com>
2023-02-14 22:23:56 +0800

    Create 1013_The_Greatest.cpp

---
+++ Beecrowd_Solutions/1013_The_Greatest.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b, c, max;
+    cin >> a >> b >> c;
+    max = a;
+    if(b>max)
+        max = b;
+    if(c>max)
+        max = c;
+    cout << max <<" eh o maior" << endl;
+    return 0;
+}
